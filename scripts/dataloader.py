d7bba406b5c7a51501a830c8c3c4d581a1958d0a
ZiadSENG <ziad.seng@gmail.com>
2024-06-23 00:15:40 +0300

    Updata the Data Loader to adapt with sequence data

--- scripts/dataloader.py
+++ scripts/dataloader.py
@@ -5,40 +5,53 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import torchvision.io as io
-import PIL
+from torch.nn.functional import one_hot
+from torch import tensor
+from PIL import Image
 
 class BreakfastActionsDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, transform=None, sequence_length=10):
         self.root_dir = root_dir
         self.transform = transform
+        self.sequence_length = sequence_length
         self.video_paths = []
         self.labels = []
         self.samples = sorted(os.listdir(root_dir))
+        self.labelDict = dict(zip(['cereals', 'coffee', 'friedegg', 'juice', 'milk', 'pancake', 'salat', 'sandwich', 'scrambledegg', 'tea'], range(10)))
         
-        self.labelDict=dict(zip(['cereals','coffee','friedegg','juice','milk','pancake','salat','sandwich','scrambledegg','tea'],range(10)))
         for person in self.samples:
-            label=person.split('_')[1]
-            label=self.labelDict[label]
-            for frame in os.listdir(os.path.join(root_dir,person)):
-                self.video_paths.append(os.path.join(root_dir,person,frame))
+            label = person.split('_')[1]
+            label = self.labelDict[label]
+            frames = sorted(os.listdir(os.path.join(root_dir, person)))
+            for i in range(0, len(frames) - self.sequence_length + 1):
+                frame_paths = frames[i:i + self.sequence_length]
+                self.video_paths.append([os.path.join(root_dir, person, frame) for frame in frame_paths])
                 self.labels.append(label)
+    
     def __len__(self):
         return len(self.video_paths)
-
+    
     def __getitem__(self, idx):
-        frame_path = self.video_paths[idx]
+        frame_paths = self.video_paths[idx]
+        frames = []
+        for frame_path in frame_paths:
+            image = Image.open(frame_path)
+            if self.transform:
+                image = self.transform(image)
+            frames.append(image)
+        frames = torch.stack(frames)
+        
         label = self.labels[idx]
-        frame= PIL.Image.open(frame_path)
+        return frames, label
 
-        if self.transform:
-            frame = self.transform(frame)
 
-        return frame, label
+    
+        
 
 # Transformations
 transform = transforms.Compose([
-    transforms.Resize((28, 28)),
-    transforms.RandomCrop((28, 28)),
+    transforms.Resize((64, 64)),
+    transforms.RandomCrop((64, 64)),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor()
 ])
@@ -46,13 +59,13 @@ transform = transforms.Compose([
 # tst = BreakfastActionsDataset('Breakfast Action Recognition\\train')
 
 
-# def create_data_loader(root_dir, batch_size=1, shuffle=False):
-#     dataset = BreakfastActionsDataset(root_dir, transform=transform)
-#     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+def create_data_loader(root_dir, batch_size=1, shuffle=False, transform=transform,sequence_length=10):
+    dataset = BreakfastActionsDataset(root_dir, transform=transform,sequence_length=sequence_length)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
 
-# df= create_data_loader('..\data\\train')
+# df= create_data_loader('..\..\data\\train')
 # for i in df:
-#     print(i)
+#     print(i[1])
 #     break
 print('DataLoader loaded')
